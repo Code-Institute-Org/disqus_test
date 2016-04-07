5de3572b7ae85b8e6168292f81509c5eda63afcc
brianojee <brian@codeinstitute.net>
2016-04-07 12:38:04 +0100

    Initial commit

---
+++ disqus_test_1_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/brianogrady/PycharmProjects/disqus_test_1/disqus_test_1_env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
